c2d900322f262350b93c7b182e384540b955280d
ahmed-H20 <ahmedheshamahah8@gmail.com>
2024-08-11 01:18:11 +0300

    Update Form

--- user_client/src/component/Signin.jsx
+++ user_client/src/component/Signin.jsx
@@ -1,18 +1,22 @@
-
+import { useForm } from "react-hook-form";
 
 const Signin = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
       <div className="hero  min-h-screen ">
         <div className="hero-content w-full">
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
-          <div className="m-8">
-          <h3 className="font-bold text-lg"> Hello MR.Hazem !</h3>
-          <p className="">
-                Please enter the password
-            </p>
-          </div>
-            <form className="card-body -mt-12">
+            <div className="m-8">
+              <h3 className="font-bold text-lg"> Hello MR.Hazem !</h3>
+              <p className="">Please enter the password</p>
+            </div>
+            <form
+              className="card-body -mt-12"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              {/* Email */}
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -22,8 +26,10 @@ const Signin = () => {
                   placeholder="email"
                   className="input input-bordered"
                   required
+                  {...register("email")}
                 />
               </div>
+              {/* Password */}
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
@@ -33,6 +39,7 @@ const Signin = () => {
                   placeholder="password"
                   className="input input-bordered"
                   required
+                  {...register("password")}
                 />
                 {/* <label className="label">
                   <a href="#" className="label-text-alt link link-hover">
